ed9a72f39f399cd2df14220fd412a1f9d2c494ee
Sojin Lee <77518932+SJ-Lee33@users.noreply.github.com>
2021-11-23 02:07:47 +0900

    Fix: 장르선택화면 intent부분 수정

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -6,22 +6,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button excitedBtn;
+    private Button funkyBtn;
+    private Button relaxBtn;
+    private Button sensualBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        excitedBtn = findViewById(R.id.excited);
-        excitedBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {//버튼을 눌렀을떄 second 이동 을 할꺼임
-                Intent intent = new Intent(MainActivity.this, MusicPlayer.class);
-                intent.putExtra("BGMgenre", v.getId());
 
-                startActivity(intent);//액티비티 이동
+        Intent intent = new Intent(MainActivity.this, MusicPlayer.class);
+
+        excitedBtn = findViewById(R.id.excited);
+        funkyBtn = findViewById(R.id.funky);
+        relaxBtn = findViewById(R.id.relax);
+        sensualBtn = findViewById(R.id.sensual);
+
+        View.OnClickListener onClickListener = new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.excited:
+                        intent.putExtra("BGMgenre", 0);
+                    case R.id.funky:
+                        intent.putExtra("BGMgenre", 1);
+                    case R.id.relax:
+                        intent.putExtra("BGMgenre", 2);
+                    case R.id.sensual:
+                        intent.putExtra("BGMgenre", 3);
+                }
+                startActivity(intent);
             }
-        });
+        };
     }
 }
